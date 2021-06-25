2206a09d9991e1d35d664dd10eb0aa4816565e59
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-25 19:23:08 -0400

    Stable

--- src/components/SectionProducts/index.tsx
+++ src/components/SectionProducts/index.tsx
@@ -1,9 +1,11 @@
+import { GalleryProducts } from "components/GalleryProducts"
 export const SectionProducts = () => {
     return (
         <section className="products">
             <div className="products__banner">
                 <img src="" alt="" />
             </div>
+            <GalleryProducts />
         </section>
     )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import { SectionProducts } from 'components/SectionProducts'
 import styles from './HomePage.module.css'
 
 export const HomePage = () => {
     return (
         <>
             <h1 className={styles.Title}>Hello World!</h1>
+            <SectionProducts />
         </>
     )
 }
