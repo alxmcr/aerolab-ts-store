5e69e1f7d62e027e88e68c6a3ebd46655ec1e4d1
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-25 18:58:57 -0400

    Section Products

---
+++ src/components/SectionProducts/index.tsx
@@ -0,0 +1,9 @@
+export const SectionProducts = () => {
+    return (
+        <section className="products">
+            <div className="products__banner">
+                <img src="" alt="" />
+            </div>
+        </section>
+    )
+}
