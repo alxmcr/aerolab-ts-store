ed1d851ed38f3f8183abb9e411993bc858f1192c
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-25 19:40:45 -0400

    Improvements

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,5 +1,5 @@
 export const getProducts = async () => {
-    return await fetch("/_data_/products.json", {
+    return await fetch("/_data_/fakeProducts.json", {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
