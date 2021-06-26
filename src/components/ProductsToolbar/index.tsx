02bc206e6e0d1db54be556d93ca073b9d3a6dab6
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-26 14:11:20 -0400

    stable productstoolbar

--- src/components/ProductsToolbar/index.tsx
+++ src/components/ProductsToolbar/index.tsx
@@ -1,3 +1,5 @@
+import arrowLeft from "assets/icons/arrow-left.svg";
+import arrowRight from "assets/icons/arrow-right.svg";
 import { useState } from "react";
 import './ProductsToolbar.css';
 
@@ -10,10 +12,18 @@ export const ProductsToolbar = () => {
             <div className="toolbar__sort">
                 <p className="toolbar__description">Sorted by:</p>
                 <div className="toolbar__buttons">
-                    <button className="toolbar__button">Most recent</button>
-                    <button className="toolbar__button">Lowest price</button>
-                    <button className="toolbar__button">Highest price</button>
+                    <button className="toolbar__button toolbar__button--filter">Most recent</button>
+                    <button className="toolbar__button toolbar__button--filter">Lowest price</button>
+                    <button className="toolbar__button toolbar__button--filter">Highest price</button>
                 </div>
+            </div>
+            <div className="toolbar__arrows">
+                <button className="toolbar__button">
+                    <img src={arrowLeft} alt="Previous page" className="toolbar__icon" />
+                </button>
+                <button className="toolbar__button">
+                    <img src={arrowRight} alt="Next page" className="toolbar__icon" />
+                </button>
             </div>
         </div>
     )
