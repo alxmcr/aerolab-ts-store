dbf52c7c7ba5a3bb120290bc1345f2a8526f720d
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-26 19:37:46 -0400

    Stable

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,14 @@
 import { CategoryHeader } from 'components/CategoryHeader'
 import { SectionProducts } from 'components/SectionProducts'
+import './HomePage.css'
 
 export const HomePage = () => {
     return (
         <>
             <CategoryHeader categoryTitle="Electronics" />
-            <SectionProducts />
+            <section className="home__container">
+                <SectionProducts />
+            </section>
         </>
     )
 }
