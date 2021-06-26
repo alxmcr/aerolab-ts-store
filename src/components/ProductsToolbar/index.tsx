c31950c67bb243d70cb968382a0a42f10134120d
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-26 14:20:07 -0400

    Create functions

--- src/components/ProductsToolbar/index.tsx
+++ src/components/ProductsToolbar/index.tsx
@@ -1,29 +1,40 @@
 import arrowLeft from "assets/icons/arrow-left.svg";
 import arrowRight from "assets/icons/arrow-right.svg";
-import { useState } from "react";
 import './ProductsToolbar.css';
 
 export const ProductsToolbar = () => {
-    const [quantityProducts, setQuantityProducts] = useState<number>(16);
-    const [quantityTotalProducts, setQuantityTotalProducts] = useState<number>(32);
+    const handleNextPage = () => { console.log("Next page..."); }
+    const handlePreviousPage = () => { console.log("Previous page..."); }
+    const handleMostRecentProducts = () => { console.log("Most Recent products..."); }
+    const handleLowestProducts = () => { console.log("Lowest products..."); }
+    const handleHighestProducts = () => { console.log("Highest products..."); }
     return (
         <div className="toolbar">
-            <p className="toolbar__status">{quantityProducts} of {quantityTotalProducts} products</p>
+            <p className="toolbar__status">16 of 32 products</p>
+            <div className="toolbar__arrows">
+                <button className="toolbar__button" onClick={handleNextPage}>
+                    <img src={arrowLeft} alt="Previous page" className="toolbar__icon" />
+                </button>
+                <button className="toolbar__button" onClick={handlePreviousPage}>
+                    <img src={arrowRight} alt="Next page" className="toolbar__icon" />
+                </button>
+            </div>
             <div className="toolbar__sort">
                 <p className="toolbar__description">Sorted by:</p>
                 <div className="toolbar__buttons">
-                    <button className="toolbar__button toolbar__button--filter">Most recent</button>
-                    <button className="toolbar__button toolbar__button--filter">Lowest price</button>
-                    <button className="toolbar__button toolbar__button--filter">Highest price</button>
+                    <button
+                        className="toolbar__button toolbar__button--filter"
+                        onClick={handleMostRecentProducts}
+                    >Most recent</button>
+                    <button
+                        className="toolbar__button toolbar__button--filter"
+                        onClick={handleLowestProducts}
+                    >Lowest price</button>
+                    <button
+                        className="toolbar__button toolbar__button--filter"
+                        onClick={handleHighestProducts}
+                    >Highest price</button>
                 </div>
-            </div>
-            <div className="toolbar__arrows">
-                <button className="toolbar__button">
-                    <img src={arrowLeft} alt="Previous page" className="toolbar__icon" />
-                </button>
-                <button className="toolbar__button">
-                    <img src={arrowRight} alt="Next page" className="toolbar__icon" />
-                </button>
             </div>
         </div>
     )
