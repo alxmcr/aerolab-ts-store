7cdbedb26e222b10d8198b0f993615d52e6e1b5c
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-26 13:44:41 -0400

    Toolbar working

--- src/components/ProductsToolbar/index.tsx
+++ src/components/ProductsToolbar/index.tsx
@@ -5,14 +5,14 @@ export const ProductsToolbar = () => {
     const [quantityProducts, setQuantityProducts] = useState<number>(16);
     const [quantityTotalProducts, setQuantityTotalProducts] = useState<number>(32);
     return (
-        <div className="filter">
-            <p className="filter__status">{quantityProducts} of {quantityTotalProducts}</p>
-            <div className="filter__sort">
-                <p className="filter__description">Sorted by</p>
-                <div className="filter__buttons">
-                    <button className="filter__button">Most recent</button>
-                    <button className="filter__button">Lowest price</button>
-                    <button className="filter__button">Highest price</button>
+        <div className="toolbar">
+            <p className="toolbar__status">{quantityProducts} of {quantityTotalProducts} products</p>
+            <div className="toolbar__sort">
+                <p className="toolbar__description">Sorted by:</p>
+                <div className="toolbar__buttons">
+                    <button className="toolbar__button">Most recent</button>
+                    <button className="toolbar__button">Lowest price</button>
+                    <button className="toolbar__button">Highest price</button>
                 </div>
             </div>
         </div>
