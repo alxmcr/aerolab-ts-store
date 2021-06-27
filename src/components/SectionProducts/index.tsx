e3ec7822571abce5dc158e6124e915e17ab68f06
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-26 22:00:28 -0400

    Improvements

--- src/components/SectionProducts/index.tsx
+++ src/components/SectionProducts/index.tsx
@@ -4,9 +4,6 @@ import { ProductsToolbar } from "components/ProductsToolbar"
 export const SectionProducts = () => {
     return (
         <section className="products">
-            <div className="products__banner">
-                <img src="" alt="" />
-            </div>
             <ProductsToolbar />
             <GalleryProducts />
         </section>
