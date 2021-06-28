956811aaec4e2d478e1afd8006c16142aded3fec
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-27 21:47:59 -0400

    Stable User Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { UserReducerState } from 'componentsTypes';
 import { AuthContext } from 'context/AuthContext';
 import { isUserAuthenticated } from 'helpers/userHelpers';
 import { useAuth } from 'hooks/useAuth';
+import { useReducer } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { userReducer } from 'reducers/userReducer';
 import { SwitchRoutesApp } from 'routing/SwitchRoutesApp';
 import styles from './App.module.css';
 
 function App() {
   const { me } = useAuth();
+  const initialUserState: UserReducerState = { me }
+  const [userState, userDispatch] = useReducer(userReducer, initialUserState);
   return (
     <BrowserRouter>
       <AuthContext.Provider value={{ me }}>
