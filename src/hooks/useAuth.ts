1dc0eb6b8174a87b165704a92be3be635d467dea
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-27 20:15:44 -0400

    + useAuth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { getMe } from "services/UserService";
+import { UserAPI } from "types";
+
+export const useAuth = () => {
+    const [me, setMe] = useState<UserAPI | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        setLoading(true);
+        setLoading(true);
+        getMe()
+            .then(response => response.json())
+            .then(data => setMe(data))
+            .catch(error => setError(error))
+            .finally(() => {
+                setLoading(false);
+                setError(null)
+            })
+    }, [])
+
+    return { me, loading, error }
+}
