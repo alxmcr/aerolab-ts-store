9e9e90404695c4c4dba08e2d9254e9c3160df9fd
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-28 22:12:07 -0400

    Change limitByPage

--- src/components/SectionProducts/index.tsx
+++ src/components/SectionProducts/index.tsx
@@ -3,7 +3,7 @@ import { GalleryProducts } from "components/GalleryProducts";
 export const SectionProducts = () => {
     return (
         <section className="products">
-            <GalleryProducts limitByPage={5} />
+            <GalleryProducts limitByPage={16} />
         </section>
     )
 }
